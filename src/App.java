a0969c346e838bd762d065c98c31d8dd20ca3d0c
mattcmerritt <mattcmerritt@gmail.com>
2021-04-12 19:10:04 -0400

    added theme to help with Mac

--- src/App.java
+++ src/App.java
@@ -326,6 +326,21 @@ public class App {
 	}
 
 	public static void main(String[] args) {
+		try {
+			for (UIManager.LookAndFeelInfo info : UIManager.getInstalledLookAndFeels()) {
+				if ("Nimbus".equals(info.getName())) {
+					UIManager.setLookAndFeel(info.getClassName());
+					break;
+				}
+			}
+		} catch (Exception e) {
+			// If Nimbus is not available, fall back to cross-platform
+			try {
+				UIManager.setLookAndFeel(UIManager.getCrossPlatformLookAndFeelClassName());
+			} catch (Exception ex) {
+				// Not worth my time
+			}
+		}
 		new App();
 	}
 
